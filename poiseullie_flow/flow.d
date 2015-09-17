9c566d77e198b6977fd89aa17e7efadde5724392
Wu Yirong <wuwuyiyirongrong@163.com>
2015-09-17 09:23:00 +0800

    check point

---
+++ poiseullie_flow/flow.d
@@ -0,0 +1,12 @@
+4	 # number of nodes #
+# Nodes which define the boundary #
+0:	-1	 -1	 0.5	 1
+1:	1	 -1	 0.5	 2
+2:	1	 1	 0.5	 4
+3:	-1	 1	 0.5	 5
+4	 # number of segments #
+# boundary segments #
+0:		0	1	1
+1:		1	2	3
+2:		2	3	4
+3:		3	0	5
